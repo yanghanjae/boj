3fbe7679f5acc66b55490a391a01571885858cfa
YHJ\dd <didgkswo123@gmail.com>
2025-07-26 23:49:53 +0900

    first commit

---
+++ 10000/10926.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String id = sc.nextLine();
+
+    System.out.println(id + "??!");
+
+    sc.close();
+
+  }
+}
